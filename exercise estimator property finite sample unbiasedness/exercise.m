d4f8722089bda5fc5a70631ca797cedcb7037e6a
Tunga Kantarcı <44529911+tungakantarci@users.noreply.github.com>
2026-01-26 00:48:33 +0100

    Commit

--- exercise estimator property finite sample unbiasedness/exercise.m
+++ exercise estimator property finite sample unbiasedness/exercise.m
@@ -1,59 +1,56 @@
-% Exercise - Understanding the consistency of the OLS estimator using simulation
+% Exercise - Understanding the unbiasedness property of the OLS estimator 
 
 %% 1. Aim of the exercise
 
-% To understand the consistency of the OLS eatimator.
+% The aim of this exercise is to understand the unbiasedness property of
+% the OLS estimator.
 
-%% 2. Theory
+%% 2. Clear the workspace   
 
-% Refer to the accompanying PDF file for the theory.
-
-%% 3. Application
-
-% 3.1. Clear the memory
+% Clear the workspace
 clear;
 
-% 3.2. Set the number of simulations 
+%% 3. Setup for the Monte Carlo simulation
+
+% 3.1. Define the sample size
+N_obs = 1000;
+
+% 3.2. Define the number of repeated samples from the population
 N_sim = 1000;
 
-% 3.3. Set the sample size
-N_obs = [1000 10000 100000];
-N_obs_j = size(N_obs,2);
+% 3.3. Generate data for the independent variable
+X = random('Uniform',-1,1,[N_obs 1]); 
 
-% 3.4. Set true values for the coefficients
-B_true = [0.2 0.5]';
+% 3.4. Define the population coefficient
+B_true = 0.5;
 
-% 3.5. Define the number of coefficients to be simulated
-N_par = 1;
+%% 4. Create the sampling distribution of the OLS estimator
 
-% 3.6. Preallocate matrices for storing the simulated statistics 
-B_hat_1_sim = NaN(N_sim,N_par);
-B_hat_1_sim_j = NaN(N_sim,N_obs_j);
+% 4.1. Preallocate matrix to store OLS estimates from repeated samples
+B_hat_sim = NaN(1,N_sim);
 
-% 3.7. Create sampling distributions for the OLS estimator 
-for j = 1:N_obs_j
-    for i = 1:N_sim 
-        u = random('Normal',0,1,[N_obs(1,j) 1]);
-        x_0 = ones(N_obs(1,j),1);
-        x_1 = random('Uniform',-1,1,[N_obs(1,j) 1]);
-        X = [x_0 x_1];
-        y = X*B_true+u;
-        LSS = exercisefunctionlss(y,X);
-        B_hat_1_sim(i,1) = LSS.B_hat(2,1);
-        B_hat_1_sim_j(:,j) = B_hat_1_sim(:,1);
-    end
-end
+% 4.2. Conduct the Monte Carlo simulation
+for i = 1:N_sim
+    u = random('Normal',0,1,[N_obs 1]);
+    y = X*B_true+u;
+    LSS = exercisefunctionlss(y,X);
+    B_hat_sim(1,i) = LSS.B_hat(1,1);
+end 
 
-%% 4. Plot the sampling distributions of the OLS estimator 
+%% 5. Plot the sampling distribution of the OLS estimator as a density
 
-% Plot
+% Figure
+figure
 hold on
-ksdensity(B_hat_1_sim_j(:,1))
-ksdensity(B_hat_1_sim_j(:,2))
-ksdensity(B_hat_1_sim_j(:,3))
-title(['Fig. 1. Sampling distribution of the OLS estimator ' ...
-    'and sample size'])
-legend('N\_obs = 1,000','N\_obs = 10,000','N\_obs = 100,000')
-ylabel('Frequency')
-xlabel('B\_hat\_1') 
+[f,xi] = ksdensity(B_hat_sim(1,:));
+plot(xi,f, ...
+    'DisplayName','Sampling distribution of B\_hat');
+line([mean(B_hat_sim(1,:)) mean(B_hat_sim(1,:))],ylim, ...
+    'DisplayName','Mean of B\_hat');
+line([B_true B_true],ylim, ...
+     'DisplayName','True B');
+title('Fig. 1. Sampling distribution of the OLS estimator and its mean')
+xlabel('B\_hat')
+ylabel('Density')
+legend('show')
 hold off
